f2d35dfc88e71f755101e27a15d386766c53b089
Wojciech Sęk <wojciech.sek17@gmail.com>
2023-05-22 23:26:19 +0200

    removed for loops

--- lab5/src/maximal_independent.rs
+++ lab5/src/maximal_independent.rs
@@ -5,52 +5,49 @@ use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time;
+use std::thread::JoinHandle;
 
+#[allow(clippy::needless_collect)]
 pub fn maximal_independent(g: &Graph) -> HashSet<usize> {
     let n = g.n;
     let independent = Arc::new(Mutex::new(HashSet::new()));
     let graph = Arc::new(Mutex::new((*g).clone()));
-    let mut processes = vec![];
 
-    for i in 0..n {
-        let graph = Arc::clone(&graph);
-        let independent = Arc::clone(&independent);
+    let processes: Vec<JoinHandle<()>> = 
+        (0..n)
+        .map(|i| {
+            let graph = Arc::clone(&graph);
+            let independent = Arc::clone(&independent);
 
-        let process = thread::spawn(move || {
-            loop {
-                let time = time::Duration::from_millis(fastrand::u64(10..100));
-                thread::sleep(time);
+            thread::spawn(move || {
+                loop {
+                    let time = time::Duration::from_millis(fastrand::u64(10..100));
+                    thread::sleep(time);
 
-                let mut independent = independent.lock().unwrap();
-                let mut graph = graph.lock().unwrap();
+                    let mut independent = independent.lock().unwrap();
+                    let mut graph = graph.lock().unwrap();
 
-                match graph.colors[i] {
-                    Color::Red => {
-                        independent.remove(&i);
-                        graph.update_color_with_neighs(&independent, i);
-                    },
-                    Color::Yellow => {
-                        independent.insert(i);
-                        graph.update_color(&independent, i);
-                    },
-                    _ => (),
+                    match graph.colors[i] {
+                        Color::Red => {
+                            independent.remove(&i);
+                            graph.update_color_with_neighs(&independent, i);
+                        },
+                        Color::Yellow => {
+                            independent.insert(i);
+                            graph.update_color(&independent, i);
+                        },
+                        _ => (),
+                    }
+
+                    if graph.colors.iter().all(|&color| color == Color::Black || color == Color::White) {
+                        break;
+                    }
                 }
-
-                if graph.colors.iter().all(|&color| color == Color::Black || color == Color::White) {
-                    break;
-                }
-            }
-        });
-        processes.push(process);
-    }
-
-    for p in processes {
-        p.join().unwrap();
-    }
-
-    let mut result = HashSet::new();
-    for &v in independent.lock().unwrap().iter() {
-        result.insert(v);
-    }
-    result
+            })
+        }).collect();
+    
+    processes.into_iter().for_each(|p| p.join().unwrap());
+   
+    let lock = Arc::try_unwrap(independent).expect("Lock still has multiple owners");
+    lock.into_inner().expect("Mutex cannot be locked")
 }
