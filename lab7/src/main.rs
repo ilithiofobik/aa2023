78af99b0915374e4a13b616a71e411b2eed13923
Wojciech Sęk <wojciech.sek17@gmail.com>
2023-06-13 00:07:52 +0200

    added randomized version

--- lab7/src/main.rs
+++ lab7/src/main.rs
@@ -3,4 +3,5 @@ mod recurr_funs;
 fn main() {
     recurr_funs::ex15();
     recurr_funs::ex16();
+    recurr_funs::ex16rand();
 }
--- lab7/src/recurr_funs.rs
+++ lab7/src/recurr_funs.rs
@@ -1,6 +1,6 @@
 use memoize::memoize;
 
-const NUM_OF_EXPS: usize = 31;
+const NUM_OF_EXPS: usize = 129;
 
 #[memoize]
 fn f_real_count(n: u32) -> u128 {
@@ -39,6 +39,29 @@ fn l_real_count(n: u32) -> u128 {
     }
 }
 
+fn l_rand_count(n: u32) -> u128 {
+    match n {
+        0 | 1 => 1,
+        n => {
+            let mut count = 1;
+            for i in 1..=n {
+                if fastrand::bool() {
+                    count += l_rand_count(i);
+                }
+            }
+            count
+        }
+    }
+}
+
+fn l_rand_count_avg(n: u32, num_of_tests: usize) -> u128 {
+    let mut sum = 0;
+    for _ in 0..num_of_tests {
+        sum += l_rand_count(n);
+    }
+    sum / num_of_tests as u128
+}
+
 
 fn l_theoretical_count(n: u32) -> u128 {
     match n {
@@ -60,4 +83,10 @@ pub fn ex16() {
             break;
         }
     }
+}
+
+pub fn ex16rand() {
+    println!("ex16 - randomized");
+    let rand_count = l_rand_count_avg(10, 1_000_000);
+    println!("rand_count for n = 10: {}", rand_count);
 }
